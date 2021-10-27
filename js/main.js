c1967c94489934e2ad4bd0506ac8ee7f4710fa7e
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2021-10-27 18:03:24 +0300

    correct $formFight

--- js/main.js
+++ js/main.js
@@ -37,12 +37,15 @@ $formFight.addEventListener('submit',  (e) => {
     player1.changeHP(enemy.value)
     player1.renderHP()
     generateLogs('hit', player2, player1, enemy.value)
+  }else {
     generateLogs('defence', player1, player2)
   }
+
   if (enemy.defence !== player.hit){
     player2.changeHP(player.value)
     player2.renderHP()
     generateLogs('hit', player1, player2, player.value )
+  }else {
     generateLogs('defence', player2, player1)
   }
   showResult()
